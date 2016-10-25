5c1e90c03d5406200faf61d98f5b2c9987d7e11b
Singh <ks575@t-cles-beast>
2016-10-25 19:31:54 +0100

    readme

---
+++ fasta_length_filter.py
@@ -0,0 +1,25 @@
+"""
+Usage: python fasta_length_filter.py <fasta file> <length>
+The script returns all the sequences with lenth greator than and equal to user specified length
+"""
+
+from __future__ import print_function
+import Bio, sys
+from Bio.Seq import Seq
+from Bio import SeqIO
+
+fasta_file = str(sys.argv[1])
+print(fasta_file)
+seq_length = int(sys.argv[2])
+print(seq_length)
+
+for s in SeqIO.parse(fasta_file, "fasta"):
+	#print(s.id)
+	#print(repr(s.seq))
+	if len(s.seq) >= seq_length:
+		print("%s%s" %(">",s.id))
+		print(s.seq)
+	else:
+		next
+
+	
