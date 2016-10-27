0cf5323ac721a66dd12a37611af7e421b2a916f4
Kumar Saurabh Singh <kumarsaurabh20@gmail.com>
2016-10-27 10:18:18 +0100

    script

--- NGS/calculate_Nx.py
+++ NGS/calculate_Nx.py
@@ -5,21 +5,42 @@ The script takes a fasta file with minium sequence to consider and Nx. The defau
 
 from __future__ import (print_function, division)
 from Bio import SeqIO
-import sys
+import sys, getopt
 
+__author__ = 'Kumar'
+
+fasta_file = ''
+min_length = int(0)
+nf = int(50)
 fasta_list = []
-fasta_file = str(sys.argv[1])
-min_length = int(sys.argv[2])
-nf = int(sys.argv[3])
 
-for s in SeqIO.parse(fasta_file, "fasta"):   
-         if min_length:   
-            if len(s.seq) >= min_length:
-               fasta_list.append(str(s.seq))
-            else:
-               next
-         else:
-            fasta_list.append(str(s.seq))   
+try:
+	myopts, args = getopt.getopt(sys.argv[1:],"f:l:n:")
+	for o, a in myopts:
+		if o == '-f':
+			fasta_file = str(a)
+		elif o == '-l':
+			min_length = int(a)
+		elif o == '-n':
+			nf = int(a)
+
+except getopt.GetoptError as e:
+   print(str(e))
+   print("Usage:: %s -f 'fasta file' -l 'minimum length' -n Nx" % sys.argv[0])
+   sys.exit(2)   
+
+try:
+	for s in SeqIO.parse(fasta_file, "fasta"):   
+		if min_length:   
+			if len(s.seq) >= min_length:
+				fasta_list.append(str(s.seq))
+			else:
+				continue
+		else:
+			fasta_list.append(str(s.seq))   
+except IOError:
+	print("Usage::python calculate_Nx.py -f 'fasta file' -l 'minimum length' -n Nx")
+	sys.exit(2)
   
 fasta_list.sort(cmp=lambda x,y: cmp(len(y),len(x)))
 
@@ -27,7 +48,7 @@ fasta_str = "".join(fasta_list)
 tot_length = len(fasta_str)
 nf_length = nf * tot_length/100
 temp_nf_list = []
-temp_sum = 0
+temp_sum = int(0)
 
 for e in fasta_list:
 	temp_nf_list.append(e)
