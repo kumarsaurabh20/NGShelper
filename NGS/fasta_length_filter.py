c47240964508695fb0a31e8d66b238bec0dcd569
Kumar Saurabh Singh <kumarsaurabh20@gmail.com>
2016-10-30 08:01:31 +0000

    script

--- NGS/fasta_length_filter.py
+++ NGS/fasta_length_filter.py
@@ -1,16 +1,31 @@
 """
-Usage: python fasta_length_filter.py <fasta file> <length>
-The script returns all the sequences with lenth greator than and equal to user specified length
+Usage: python fasta_length_filter.py -f <fasta file> -l <length>
+The script returns all the sequences with length greator than or equal to user specified length
 """
 from __future__ import print_function
 import sys
 from Bio import SeqIO
 
+fasta_file = ''
+min_length = 1000
 
-for s in SeqIO.parse(str(sys.argv[1]), "fasta"):
-	if len(s.seq) >= int(sys.argv[2]):
-		print("%s%s" %(">",s.id))
-		print(s.seq)
-	else:
-		next
-	
+try:
+   myopts, args = getopt.getopt(sys.argv[1:],"f:l:")
+except getopt.GetoptError as e:
+   print(str(e))
+   print("Usage:: %s -f 'fasta file' -l 'minimum length'" % sys.argv[0])
+   sys.exit(2)   
+for o, a in myopts:
+   if o == '-f':
+      fasta_file = str(a)
+   elif o == '-l':
+      min_length = int(a)
+try:
+	for s in SeqIO.parse(str(fasta_file), "fasta"):
+		if len(s.seq) >= int(min_length):
+			print("%s%s" %(">",s.id))
+			print(s.seq)
+		else:
+			next
+except IOError:
+	print("Usage:: %s -f 'fasta file' -l 'minimum length'" % sys.argv[0])	
