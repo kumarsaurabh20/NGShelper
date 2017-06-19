83da5d7855f706f12ed52931afe3c2013d9ce0ed
Kumar Saurabh Singh <kumarsaurabh20@gmail.com>
2017-06-19 15:41:15 +0100

    vcf2sf

--- NGS/vcf2sf.py
+++ NGS/vcf2sf.py
@@ -14,18 +14,6 @@ n = int(0)
 position = int(0)
 fold = int()
 
-#file = vcf.Reader(open('test.vcf','r'))
-#record = next(file)
-#print(record.INFO['DP'])
-#print(record.samples)
-
-#vcf_reader = vcf.Reader(open(r'path\to\zebrafish.vcf', 'rb'))
-#for record in vcf_reader:
-#	for call in record.samples:
-#		if call.sample=='TUB' or call.sample=='TA':
-#			if call.gt_nums != '0/0':
-#				print call.gt_nums
-
 try:
 	myopts, args = getopt.getopt(sys.argv[1:],"f:s:")
 	for o, a in myopts:
@@ -65,12 +53,3 @@ for record in vcf_reader:
 	else:
 		sf.write("%d\t%d\t%d\t%d\n"% (position, x, n, fold))
 sf.close()
-		
-	
-
-	#print(isinstance(ad, list))
-	#print(record.FORMAT)	
-	#print(record.genotype('LIB15046').data[0:]) #worked for getting sample specific information but the list is unordered/not constant
-	#for sample in record.samples:
-		#print(sample)
-
