2dcaab6aeb17b5e99cd43f1e353b9b2b90f81818
Kumar Saurabh Singh <kumarsaurabh20@gmail.com>
2017-09-07 12:04:29 +0100

    update

--- PopulationGenomics/Popoolation_analysis/matrix_of_fst_means.sh
+++ PopulationGenomics/Popoolation_analysis/matrix_of_fst_means.sh
@@ -17,11 +17,13 @@ num_columns=$samples
 #
 total_cells=$(($samples**2/2-$samples/2))
 echo "$total_cells"
+range=$(($total_cells + 5))
 #val=$total_cells - 1
 #
 #Filter the fst file by removing intial 5 colums and retaining all the comparison
-#cut -f6-71 myzus_all_samples_ws_100.fst > myzus_all_samples_ws_100_filtered.fst
-
+cut -f6-$range $fstfile > filtered.fst
+file="filtered.fst"
+#
 function ProgressBar {
         let _progress=(${1}*100/${2}*100)/100
         let _done=(${_progress}*4)/10
@@ -30,14 +32,14 @@ function ProgressBar {
         _left=$(printf "%${_left}s")
 printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
 }
-
+#
 y=1
 for ((i=1;i<=num_rows;i++)) do
     #echo "this is outer loop"
     for ((j=1+$i;j<=num_columns;j++)) do
 	ProgressBar $y $total_cells
 	#echo "this is inner loop"			
- 	mean=`cut -f $y $fstfile | cut -d '=' -f 2 | awk '{ total += $1 } END { print total/NR }' -`
+ 	mean=`cut -f $y $file | cut -d '=' -f 2 | awk '{ total += $1 } END { print total/NR }' -`
 	#echo "mean for cell $y: $mean"
  	matrix[$i,$j]=$mean
  	y=$(($y+1))
@@ -47,7 +49,8 @@ done
 f1="%$((${#num_rows}+1))s"
 f2=" %9s"
 #printf "$f1" ''
-for ((i=1;i<=num_rows;i++)) do
+echo
+for ((i=1;i<=num_rows;i++)) do	
     printf "$f2" $i
 done
 echo
