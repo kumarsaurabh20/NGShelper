5dc0beb7ffd28c6ea03ad394b790ca6c8506ba22
ks575 <ks575@athena-l01.cm.cluster>
2020-09-25 19:05:53 +0100

    Nx

--- NGS/calculate_Nx.py
+++ NGS/calculate_Nx.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 """
 Usage:python calculate_Nx.py <fasta_file> <minimum_length> <Nx>
 The script takes a fasta file with minium sequence to consider and Nx. The default minimum sequence is 0 and Nx is N50.
@@ -7,6 +7,7 @@ The script takes a fasta file with minium sequence to consider and Nx. The defau
 from __future__ import (print_function, division)
 from Bio import SeqIO
 import sys, getopt
+import numpy
 
 __author__ = 'Kumar'
 
@@ -43,23 +44,53 @@ except IOError:
 	print("Usage::python calculate_Nx.py -f 'fasta file' -l 'minimum length' -n Nx")
 	sys.exit(2)
   
-fasta_list.sort(cmp=lambda x,y: cmp(len(y),len(x)))
-
+#fasta_list.sort(lambda x,y: cmp(len(y),len(x)))
+fasta_list.sort(key=len, reverse=True)
 fasta_str = "".join(fasta_list)
 tot_length = len(fasta_str)
 nf_length = nf * tot_length/100
 temp_nf_list = []
 temp_sum = int(0)
 
+#Calculate other sequence stats
+shortest = len(fasta_list[-1])
+largest = len(fasta_list[0])
+GC_content = float((fasta_str.count('G') + fasta_str.count('C'))) / tot_length * 100
+mean_len = numpy.mean([len(x) for x in fasta_list])
+median_len = numpy.median([len(x) for x in fasta_list])
+#Calculate N50 and L50
+count = 0
 for e in fasta_list:
 	temp_nf_list.append(e)
 	temp_sum += len(e)
 	if temp_sum >= nf_length:
-		nf_seq = int(len(e))
+		n50 = int(len(e))
+		l50 = count
 		break
 	else:
+		count += 1
 		continue 
 
-print("N%i: %i" %(nf,nf_seq))
-print("#Sequences: %i" %(len(fasta_list)))
+print(" ")
+print("########## Assembly Statistics ##########")
+print(" ")
+print("N%i: %i" %(nf,n50))
+print(" ")
+print("L%i: %i" %(nf,l50))
+print(" ")
+print("Shortest seq: %i" %(shortest))
+print(" ")
+print("Longest seq: %i" %(largest))
+print(" ")
+print("Mean length: %i" %(round(mean_len)))
+print(" ")
+print("Median length: %i" %(median_len))
+print(" ")
+print("Total sequences: %i" %(len(fasta_list)))
+print(" ")
 print("Total bases: %i" %(tot_length))
+print(" ")
+print("GC content: %f" %(GC_content))
+print(" ")
+print("############################################")
+print(" ")
